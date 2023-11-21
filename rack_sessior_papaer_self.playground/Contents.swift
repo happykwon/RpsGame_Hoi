9d81888a8232ec217475fe714777056fdf8858f5
Hoi.Kwon <teaho177@gmail.com>
2023-11-22 00:17:47 +0900

    커맨드라인 파일로 재업로드

--- rack_sessior_papaer_self.playground/Contents.swift
+++ rack_sessior_papaer_self.playground/Contents.swift
@@ -6,17 +6,38 @@ enum Rps: Int {
     case paper = 3
 }
 
-let userInput = [1,2,3]
 
 func startGame() {
     print("가위(1), 바위(2), 보(3) ! <종료 : 0> :" )
     
-    if let userInput = readLine(), let userChoice = Int(userInput) {
-         
-    } else {
+    guard let Input = readLine(), let userChoice = Int(Input) else {
+        print("잘못된 입력입니다. 다시 시도해주세요")
+        startGame()
+        return
+    }
+    
+    if userChoice == 0 {
+        print("게임을 종료합니다.")
+    } else if userChoice < 1 || userChoice > 3  {
         print("잘못된 입력입니다. 다시 시도해주세요")
     }
     
+    var computerChoice = Int.random(in: 1...3)
     
-
+    if userChoice == computerChoice {
+        print("비겼습니다!")
+        startGame()
+    } else if
+        (userChoice == 1 && computerChoice == 3) ||
+        (userChoice == 2 && computerChoice == 1) ||
+        (userChoice == 3 && computerChoice == 1) {
+        print("이겼습니다!")
+    } else {
+        print("졌습니다.")
+    }
+    
 }
+
+startGame()
+
+
---
+++ rpsGame_Hoi/rpsGame_Hoi/main.swift
@@ -0,0 +1,49 @@
+//
+//  main.swift
+//  rpsGame_Hoi
+//
+//  Created by 권태호 on 22/11/2023.
+//
+
+import Foundation
+
+import Foundation
+
+
+
+while true {
+    func startGame() {
+        print("가위(1), 바위(2), 보(3) ! <종료 : 0> :" )
+        
+        guard let Input = readLine(), let userChoice = Int(Input) else {
+            print("잘못된 입력입니다. 다시 시도해주세요")
+            startGame()
+            return
+        }
+        
+        if userChoice == 0 {
+            print("게임을 종료합니다.")
+        } else if userChoice < 1 || userChoice > 3  {
+            print("잘못된 입력입니다. 다시 시도해주세요")
+        }
+        
+        var computerChoice = Int.random(in: 1...3)
+        
+        if userChoice == computerChoice {
+            print("비겼습니다!")
+            startGame()
+        } else if
+            (userChoice == 1 && computerChoice == 3) ||
+            (userChoice == 2 && computerChoice == 1) ||
+            (userChoice == 3 && computerChoice == 1) {
+            print("이겼습니다!")
+        } else {
+            print("졌습니다.")
+        }
+        
+    }
+    startGame()
+}
+
+
+
