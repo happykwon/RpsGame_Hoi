242c6f90dde476d22df402b002ef595302a241ea
Hoi.Kwon <teaho177@gmail.com>
2023-11-21 23:40:21 +0900

    유저 초이스

--- rack_sessior_papaer_self.playground/Contents.swift
+++ rack_sessior_papaer_self.playground/Contents.swift
@@ -1,24 +1,22 @@
 import Foundation
 
-
-//묵찌빠
-
 enum Rps: Int {
     case rock = 1
     case sissors = 2
     case paper = 3
 }
 
+let userInput = [1,2,3]
 
-var text: String {
-    switch {
-    case Rps.rock:
-        print
+func startGame() {
+    print("가위(1), 바위(2), 보(3) ! <종료 : 0> :" )
+    
+    if let userInput = readLine(), let userChoice = Int(userInput) {
+         
+    } else {
+        print("잘못된 입력입니다. 다시 시도해주세요")
     }
+    
+    
+
 }
-
-
-
-
-
-
