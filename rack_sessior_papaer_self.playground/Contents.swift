6e0b78a76be97aaf6acee9ae85b2cc9c5bd9876e
Hoi.Kwon <teaho177@gmail.com>
2023-11-21 22:03:39 +0900

    First commit

---
+++ rack_sessior_papaer_self.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+
+//묵찌빠
+
+enum Rps: Int {
+    case rock = 1
+    case sissors = 2
+    case paper = 3
+}
+
+
+var text: String {
+    switch {
+    case Rps.rock:
+        print
+    }
+}
+
+
+
+
+
+
